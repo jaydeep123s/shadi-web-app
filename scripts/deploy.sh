79b74aa93629b25be1b931126d4367fa0cf596b2
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-16 22:27:20 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,40 @@
-# scripts/build.sh
+# scripts/deploy.sh
 #!/bin/bash
+
+# Print a message indicating the start of the deployment
+echo "Starting deployment..."
+
+# Navigate to the application directory or create it if it doesn't exist
+if [ ! -d "/home/ubuntu/shadi-web-app" ]; then
+    mkdir -p /home/ubuntu/shadi-web-app
+fi
+
+cd /home/ubuntu/shadi-web-app
+
+# Pull the latest code from the repository
+git pull origin main
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Print build success message
-echo "Build successful!"
+# Configure Nginx
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOL
+server {
+    listen 80;
+
+    location / {
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+}
+EOL
+
+# Restart Nginx to apply the changes
+sudo systemctl restart nginx
+
+# Run the application
+nohup python3 app/app.py &
