36806d223c3f48b97c3a9fca0d6ccf9a952c3485
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-16 22:25:29 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,1 +1,17 @@
+# tests/test_app.py
+import unittest
+from app.app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Welcome to Shadi.com")
+
+if __name__ == '__main__':
+    unittest.main()
 
